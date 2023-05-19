2192778210000fe45b70cd0f1563f4238c3271f4
qzhang16 <qzhang16@ra.rockwell.com>
2023-05-19 10:05:33 +0800

    intial setup from springboot wizard in vscode.

---
+++ src/main/java/asg/dev/abc28/Abc28Application.java
@@ -0,0 +1,13 @@
+package asg.dev.abc28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Abc28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Abc28Application.class, args);
+	}
+
+}
---
+++ src/test/java/asg/dev/abc28/Abc28ApplicationTests.java
@@ -0,0 +1,13 @@
+package asg.dev.abc28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Abc28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
